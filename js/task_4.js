df6abf91f73ce95032b152a5554140e4975a1be2
r-strilets <str.komar@gmail.com>
2022-08-25 19:10:21 +0300

    add task_4

---
+++ js/task_4.js
@@ -0,0 +1,21 @@
+// Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
+// Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
+// Оновлюй інтерфейс новим значенням змінної counterValue.
+const counterTextСontent = document.querySelector("#value");
+const decrementButton = document.querySelector('[data-action="decrement"]');
+const incrementButton = document.querySelector('[data-action="increment"]');
+
+decrementButton.addEventListener("click", clickOnDecrement);
+incrementButton.addEventListener("click", clickOnIncrement);
+
+let incrementValue = 0;
+
+function clickOnDecrement(event) {
+  incrementValue -= 1;
+  counterTextСontent.textContent = incrementValue;
+}
+
+function clickOnIncrement(event) {
+  incrementValue += 1;
+  counterTextСontent.textContent = incrementValue;
+}
