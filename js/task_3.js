db3d9ee61e9c238b6bcab2113ddcb169d819a57d
r-strilets <55804526+r-strilets@users.noreply.github.com>
2022-08-25 17:54:52 +0300

    added Task3

---
+++ js/task_3.js
@@ -0,0 +1,29 @@
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
+function createJallery() {
+  const newGallery = images.reduce((acc, { url, alt }) => {
+    return (acc += `<li><img src="${url}" alt="${alt}"/></li>`);
+  }, "");
+
+  document
+    .querySelector(".gallery")
+    .insertAdjacentHTML("afterbegin", newGallery);
+}
+createJallery();
