540c6f85ebb354fa57294d3e5abdeb4ffa865ddd
r-strilets <str.komar@gmail.com>
2022-08-25 11:53:29 +0300

    first task

---
+++ js/task_1.js
@@ -0,0 +1,8 @@
+const listItem = document.querySelectorAll(".item");
+const totalOfCategories = () =>
+  console.log(`Number of categories: ${listItem.length}`);
+totalOfCategories();
+
+function categoryOfList() {}
+
+categoryOfList();
