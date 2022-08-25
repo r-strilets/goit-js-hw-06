4ee93599f7be1abaddeff9527e4489ecb11de500
r-strilets <55804526+r-strilets@users.noreply.github.com>
2022-08-25 14:04:03 +0300

    done first tast

--- js/task_1.js
+++ js/task_1.js
@@ -1,8 +1,15 @@
 const listItem = document.querySelectorAll(".item");
+
 const totalOfCategories = () =>
   console.log(`Number of categories: ${listItem.length}`);
 totalOfCategories();
 
-function categoryOfList() {}
+function categoryOfList() {
+  listItem.forEach((item) => {
+    console.log(
+      `\nCategory: ${item.firstElementChild.textContent}\nElements: ${item.lastElementChild.children.length}`
+    );
+  });
+}
 
 categoryOfList();
